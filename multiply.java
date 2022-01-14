988c80c7846881d41f787ef48f4402b376094160
Ankit kumar <ankitsoni7544@gmail.com>
2022-01-14 23:12:03 +0530

    all files are commited

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a=7;
+        int b=3;
+        System.out.println("the multiplication of given two no. is "+(a*b));
+    }
+    
+}
